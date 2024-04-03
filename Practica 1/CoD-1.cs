b066aa786fd9eabfdeb92bccd482b0b978849c6e
z1eroRoma <artemievroma67@gmail.com>
2024-04-03 09:40:19 +0700

    four commit

--- Practica 1/CoD-1.cs
+++ Practica 1/CoD-1.cs
@@ -1,41 +1,6 @@
-    
-using System;
-
-class Program
+static double CalculateSum(double a, double b)
 {
-    static void Main(string[] args)
-    {
-        GreetUser();
-        string username = GetUserName();
-        Console.WriteLine($"Привет, {username}!");
-        DisplaySum();
-    }
-static void GreetUser()
-{
-    // Функция приветствия пользователя
-    Console.WriteLine("Привет! Добро пожаловать в наше приложение.");
+    // Функция для вычисления суммы двух чисел
+    return a + b;
 }
-
-static string GetUserName()
-{
-    // Функция для запроса имени пользователя
-    Console.Write("Пожалуйста, введите ваше имя: ");
-    string name = Console.ReadLine();
-    return name;
 }
-
-static void DisplaySum()
-{
-    // Функция для запроса двух чисел у пользователя и вывода их суммы
-    Console.Write("Введите первое число: ");
-    double num1 = Convert.ToDouble(Console.ReadLine());
-
-    
-    Console.Write("Введите второе число: ");
-    double num2 = Convert.ToDouble(Console.ReadLine());
-
-    double result = CalculateSum(num1, num2);
-    Console.WriteLine($"Сумма введенных чисел: {result}");
-}
-
-    
